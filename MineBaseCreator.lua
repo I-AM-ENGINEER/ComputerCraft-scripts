4cf7b0170779fdd4ce9d637f5fee358405897be7
I-AM-ENGINEER <61810711+I-AM-ENGINEER@users.noreply.github.com>
2025-05-08 14:12:29 +0300

    .

--- MineBaseCreator.lua
+++ MineBaseCreator.lua
@@ -1,21 +1,30 @@
+-- Reliable function to move forward, breaking blocks if necessary
+function moveForwardReliable()
+    while not turtle.forward() do
+        turtle.dig()
+        sleep(0.5) -- Small delay to avoid infinite loops
+    end
+end
+end
+
 -- Function to create a vertical tunnel section 5x5 using a ComputerCraft turtle
 function dig5x5Layer()
     for x = 1, 5 do
         for y = 1, 5 do
             turtle.digDown()
             if y < 5 then
-                turtle.forward()
+                moveForwardReliable()
             end
         end
         -- Move back to the start of the row
         if x < 5 then
             if x % 2 == 1 then
                 turtle.turnRight()
-                turtle.forward()
+                moveForwardReliable()
                 turtle.turnRight()
             else
                 turtle.turnLeft()
-                turtle.forward()
+                moveForwardReliable()
                 turtle.turnLeft()
             end
         end
@@ -30,7 +39,6 @@ function dig5x5Layer()
     end
     turtle.turnLeft()
 end
-
 function createVerticalTunnel(height)
     for i = 1, height do
         dig5x5Layer()
